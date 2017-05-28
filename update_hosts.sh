200b0367c279ba2b7b1f61f0e035288d498b7598
liuyaya <ticmers@gmail.com>
2017-05-29 00:32:32 +0800

    add update_hosts

---
+++ update_hosts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+hosts_file='/etc/hosts'
+local_end='###local_end###'
+tmp_file='/tmp/tmp_hosts'
+echo "Start execute"
+
+if [ -f "$tmp_file" ]; then
+	rm -rf $tmp_file
+fi
+line=`sed -n "/^${local_end}$/=" ${hosts_file}`
+if [ ! $line ]; then
+	echo "not found '${local_end}'"
+	exit 1
+fi
+sed -n "1,${line}p" $hosts_file > $tmp_file
+echo "\n" >> $tmp_file
+curl https://raw.githubusercontent.com/racaljk/hosts/master/hosts 2>/dev/null >> $tmp_file
+cat $tmp_file > $hosts_file
+rm -rf $tmp_file
+echo "End execute"
